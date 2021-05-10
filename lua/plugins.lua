749b477bf3cb9b05000a3c7793e764321378d23a
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-05-10 16:01:57 -0700

    first commit

---
+++ lua/plugins.lua
@@ -0,0 +1,146 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    execute "packadd packer.nvim"
+end
+
+--- Check if a file or directory exists in this path
+local function require_plugin(plugin)
+    local plugin_prefix = fn.stdpath("data") .. "/site/pack/packer/opt/"
+
+    local plugin_path = plugin_prefix .. plugin .. "/"
+    --	print('test '..plugin_path)
+    local ok, err, code = os.rename(plugin_path, plugin_path)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
+    end
+    --	print(ok, err, code)
+    if ok then
+        vim.cmd("packadd " .. plugin)
+    end
+    return ok, err, code
+end
+
+vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
+
+return require("packer").startup(
+    function(use)
+        -- Packer can manage itself as an optional plugin
+        use "wbthomason/packer.nvim"
+        use {
+            "vimwiki/vimwiki",
+            config = function()
+                local wiki = {}
+
+                wiki.path = "$HOME/.vimwiki"
+
+                wiki.syntax = "markdown"
+
+                wiki.ext = ".md"
+
+                wiki.list_margin = 0
+                wiki.auto_toc = 1
+                wiki.auto_generate_links = 1
+
+                local wiki2 = {}
+                wiki2.path = "~/Dropbox/HinduismWiki"
+                wiki2.path_html = "~/Dropbox/HinduismWiki/html"
+                wiki2.syntax = "markdown"
+                wiki2.ext = ".md"
+                wiki2.list_margin = 0
+                wiki2.auto_toc = 1
+                wiki2.auto_generate_links = 1
+
+                vim.g.vimwiki_list = {
+                    wiki,
+                    wiki2
+                }
+                vim.g.vimwiki_autowriteall = 1
+                vim.g.vimwiki_global_ext = 0
+                vim.g.vimwiki_use_mouse = 1
+                vim.g.vimwiki_folding = "expr"
+                vim.g.vimwiki_auto_chdir = 1
+                vim.g.vimwiki_table_mappings = 0
+            end
+        }
+        -- TODO refactor all of this (for now it works, but yes I know it could be wrapped in a simpler function)
+        use {"neovim/nvim-lspconfig", opt = true}
+        use {"glepnir/lspsaga.nvim", opt = true}
+        use {"kabouzeid/nvim-lspinstall", opt = true}
+        use {"nekonako/xresources-nvim"}
+        -- Telescope
+        use {"nvim-lua/popup.nvim", opt = true}
+        use {"nvim-lua/plenary.nvim", opt = true}
+        use {"nvim-telescope/telescope.nvim", opt = true}
+        use {"nvim-telescope/telescope-fzy-native.nvim", opt = true}
+
+        -- Debugging
+        use {"mfussenegger/nvim-dap", opt = true}
+
+        -- Autocomplete
+        use {"hrsh7th/nvim-compe", opt = true}
+        use {"hrsh7th/vim-vsnip", opt = true}
+        use {"rafamadriz/friendly-snippets", opt = true}
+
+        -- Treesitter
+        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+        use {"windwp/nvim-ts-autotag", opt = true}
+
+        -- Explorer
+        use {"kyazdani42/nvim-tree.lua", opt = true}
+        -- TODO remove when open on dir is supported by nvimtree
+        use "kevinhwang91/rnvimr"
+
+        -- use {'lukas-reineke/indent-blankline.nvim', opt=true, branch = 'lua'}
+        use {"lewis6991/gitsigns.nvim", opt = true}
+        -- use {"liuchengxu/vim-which-key", opt = true}
+        use {"folke/which-key.nvim", opt = true}
+        use {"ChristianChiarulli/dashboard-nvim", opt = true}
+        use {"windwp/nvim-autopairs", opt = true}
+        use {"terrortylor/nvim-comment", opt = true}
+        use {"kevinhwang91/nvim-bqf", opt = true}
+
+        -- Color
+        use {"christianchiarulli/nvcode-color-schemes.vim", opt = true}
+
+        -- Icons
+        use {"kyazdani42/nvim-web-devicons", opt = true}
+
+        -- Status Line and Bufferline
+        use {"glepnir/galaxyline.nvim", opt = true}
+        use {"romgrk/barbar.nvim", opt = true}
+
+        require_plugin("vimwiki")
+        require_plugin("xresources-nvim")
+        require_plugin("nvim-lspconfig")
+        require_plugin("lspsaga.nvim")
+        require_plugin("nvim-lspinstall")
+        require_plugin("friendly-snippets")
+        require_plugin("popup.nvim")
+        require_plugin("plenary.nvim")
+        require_plugin("telescope.nvim")
+        require_plugin("nvim-dap")
+        require_plugin("nvim-compe")
+        require_plugin("vim-vsnip")
+        require_plugin("nvim-treesitter")
+        require_plugin("nvim-ts-autotag")
+        require_plugin("nvim-tree.lua")
+        require_plugin("gitsigns.nvim")
+        require_plugin("which-key.nvim")
+        require_plugin("dashboard-nvim")
+        require_plugin("nvim-autopairs")
+        require_plugin("nvim-comment")
+        require_plugin("nvim-bqf")
+        require_plugin("nvcode-color-schemes.vim")
+        require_plugin("nvim-web-devicons")
+        require_plugin("galaxyline.nvim")
+        require_plugin("barbar.nvim")
+    end
+)
