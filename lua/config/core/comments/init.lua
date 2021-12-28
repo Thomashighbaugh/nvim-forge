30b1b5ca69f361c27cc0e84e3064150d9fc063be
Thomas Highbaugh <thighbaugh@zoho.com>
2021-12-27 22:46:18 -0800

    --- Auto-Git Commit ---

--- lua/config/core/comments/init.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-vim.g.kommentary_create_default_mappings = false
-
-vim.api.nvim_set_keymap("n", "<leader>cc", "<Plug>kommentary_line_default", {})
-vim.api.nvim_set_keymap("n", "<leader>c", "<Plug>kommentary_motion_default", {})
-vim.api.nvim_set_keymap("x", "<leader>c", "<Plug>kommentary_visual_default", {})
-
-return M
