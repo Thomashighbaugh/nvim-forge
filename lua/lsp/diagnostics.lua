5f60cf4ca47b566f354e59d8d917e1d07671bb6d
Thomas Highbaugh <thighbaugh@zoho.com>
2021-12-28 00:39:06 -0800

    --- Auto-Git Commit ---

--- lua/lsp/diagnostics.lua
+++ lua/lsp/diagnostics.lua
@@ -1,62 +1,38 @@
-local icons = require("plugins.theme.icons")
-local M = {}
+local utils = require('cosmic.utils')
+local icons = require('cosmic.theme.icons')
+local config = require('config')
 
-function M.init()
-  vim.diagnostic.config(
-    {
-      underline = true,
-      update_in_insert = false,
-      virtual_text = {
-        spacing = 4,
-        source = "always",
-        severity = {
-          min = vim.diagnostic.severity.HINT
-        }
-      },
-      signs = true,
-      severity_sort = true,
-      float = {
-        show_header = false,
-        source = "always",
-        border = "single"
-      }
-    }
-  )
+-- set up LSP signs
+local signs = {
+  Error = icons.error .. ' ',
+  Warn = icons.warn .. ' ',
+  Hint = icons.hint .. ' ',
+  Info = icons.info .. ' ',
+}
 
-  local function do_diagnostic_signs()
-    local signs = {
-      Error = icons.error .. " ",
-      Warn = icons.warn .. " ",
-      Hint = icons.hint .. " ",
-      Info = icons.info .. " "
-    }
-
-    local t = vim.fn.sign_getdefined("DiagnosticSignWarn")
-    if vim.tbl_isempty(t) then
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
-      end
-    end
-  end
-  local function do_legacy_diagnostic_signs()
-    local signs = {
-      Error = icons.error .. " ",
-      Warning = icons.warn .. " ",
-      Hint = icons.hint .. " ",
-      Information = icons.info .. " "
-    }
-    local h = vim.fn.sign_getdefined("LspDiagnosticsSignWarn")
-    if vim.tbl_isempty(h) then
-      for type, icon in pairs(signs) do
-        local hl = "LspDiagnosticsSign" .. type
-        vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
-      end
-    end
-  end
-
-  do_diagnostic_signs()
-  do_legacy_diagnostic_signs()
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 end
 
-return M
+-- set up vim.diagnostics
+-- vim.diagnostic.config opts
+vim.diagnostic.config(utils.merge({
+  underline = true,
+  signs = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = config.border,
+    focusable = false,
+    header = { icons.debug .. ' Diagnostics:', 'Normal' },
+    source = 'always',
+  },
+  virtual_text = {
+    spacing = 4,
+    source = 'always',
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+    },
+  },
+}, config.diagnostic or {}))
