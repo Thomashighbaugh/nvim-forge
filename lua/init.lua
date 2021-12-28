00b117af67321f681ac6e4f03b1a32677ed4a303
Thomas Highbaugh <thighbaugh@zoho.com>
2021-12-27 23:00:55 -0800

    --- Auto-Git Commit ---

--- lua/init.lua
+++ lua/init.lua
@@ -1,4 +1,19 @@
-require("core")
-require("pluginsInit")
-require("compiled")
-require("mappings")
+local mods = {
+    require("core"),
+    require("compiled")
+}
+
+for _, mod in ipairs(mods) do
+    local ok, err = pcall(require, mod)
+    if mod == "cosmic.compiled" and not ok then
+        vim.notify(
+            "Run :PackerCompile!",
+            vim.log.levels.WARN,
+            {
+                title = "CosmicNvim"
+            }
+        )
+    elseif not ok and not mod:find("cosmic.config") then
+        error(("Error loading %s...\n\n%s"):format(mod, err))
+    end
+end
