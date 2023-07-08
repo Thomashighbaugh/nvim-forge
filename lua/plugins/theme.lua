ad2842ca41f83d3e32c13e6a28a906b38f268f78
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-07-07 21:37:15 -0700

    I swear I am not spam and will move to codeberg

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,39 +1,8 @@
-local settings = require("core.settings")
-
-if settings.theme == "tundra" then
-	return {
-		"sam4llis/nvim-tundra",
-		config = function()
-			require("plugins.themes.tundra")
-		end,
-	}
-elseif settings.theme == "kanagawa" then
-	return {
-		"rebelot/kanagawa.nvim",
-		config = function()
-			require("plugins.themes.kanagawa")
-		end,
-	}
-elseif settings.theme == "vscode" then
-	return {
-		"Mofiqul/vscode.nvim",
-		config = function()
-			require("plugins.themes.vscode")
-		end,
-	}
-elseif settings.theme == "nightfly" then
-	return {
-		"bluz71/vim-nightfly-colors",
-		name = "nightfly",
-		config = function()
-			require("plugins.themes.nightfly")
-		end,
-	}
-else
-	return {
-		"EdenEast/nightfox.nvim",
-		config = function()
-			require("plugins.themes.nightfox")
-		end,
-	}
-end
+-- At the end of the day, I like my own brand
+-- FEATURING: My base16 colorscheme "Vice"!
+return {
+  "RRethy/nvim-base16",
+  config = function()
+    vim.cmd("colorscheme base16-vice")
+  end,
+}
