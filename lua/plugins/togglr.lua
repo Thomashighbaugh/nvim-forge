61cd5df73018a6982942ee20fd699020fafb5320
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-08-04 12:10:39 -0700

    correct keybinding for togglr

--- lua/plugins/togglr.lua
+++ lua/plugins/togglr.lua
@@ -1,6 +1,20 @@
 M = {
     "elentok/togglr.nvim",
-    opts = {},
+    opts = {
+        -- Specify key map (set to false or nil to disable)
+        key = "<Leader>tg",
+
+        -- Specify which register to use (to avoid overriding the default register)
+        register = "t",
+
+        -- Enable debugging mode
+        debug = true,
+
+        -- Add custom sets to values to toggle between
+        values = {
+            ["value"] = "opposite-value",
+        },
+    },
 }
 
 return M
