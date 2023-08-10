989806ab08bdcc6c7345529e0a83bb738b2fb094
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-08-10 10:43:47 -0700

    make sure this is updated before disaster strikes

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+require("utils.notes").setup()
 
 -- Remap for dealing with visual line wraps
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
@@ -21,14 +22,19 @@ map("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 
 -- Cancel search highlighting with ESC
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear hlsearch and ESC" })
+map(
+    { "i", "n" },
+    "<esc>",
+    "<cmd>noh<cr><esc>",
+    { desc = "Clear hlsearch and ESC" }
+)
 
 -- Sort selected lines alphabetically
 map("v", "<C-p>", "<cmd>Sort <cr>", { desc = "Sort Lines Alphabetically" })
 
 -- move over a closing element in insert mode
 map("i", "<C-l>", function()
-	return require("utils.functions").escapePair()
+    return require("utils.functions").escapePair()
 end)
 
 -- search like you are used to
@@ -39,38 +45,63 @@ map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- toggles
 map("n", "<leader>th", function()
-	vim.o.list = vim.o.list == false and true or false
+    vim.o.list = vim.o.list == false and true or false
 end, { desc = "Toggle hidden chars" })
 map("n", "<leader>tl", function()
-	vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
+    vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
 end, { desc = "Toggle signcolumn" })
 map("n", "<leader>tv", function()
-	vim.o.virtualedit = vim.o.virtualedit == "all" and "block" or "all"
+    vim.o.virtualedit = vim.o.virtualedit == "all" and "block" or "all"
 end, { desc = "Toggle virtualedit" })
 map("n", "<leader>ts", function()
-	vim.o.spell = vim.o.spell == false and true or false
+    vim.o.spell = vim.o.spell == false and true or false
 end, { desc = "Toggle spell" })
 map("n", "<leader>tw", function()
-	vim.o.wrap = vim.o.wrap == false and true or false
+    vim.o.wrap = vim.o.wrap == false and true or false
 end, { desc = "Toggle wrap" })
 map("n", "<leader>tc", function()
-	vim.o.cursorline = vim.o.cursorline == false and true or false
+    vim.o.cursorline = vim.o.cursorline == false and true or false
 end, { desc = "Toggle cursorline" })
-map("n", "<leader>to", "<cmd>lua require('utils.functions').toggle_colorcolumn()<cr>", { desc = "Toggle colorcolumn" })
 map(
-	"n",
-	"<leader>tt",
-	"<cmd>lua require('plugins.lsp.utils').toggle_virtual_text()<cr>",
-	{ desc = "Toggle Virtualtext" }
+    "n",
+    "<leader>to",
+    "<cmd>lua require('utils.functions').toggle_colorcolumn()<cr>",
+    { desc = "Toggle colorcolumn" }
 )
-map("n", "<leader>tS", "<cmd>SymbolsOutline<cr>", { desc = "Toggle SymbolsOutline" })
+map(
+    "n",
+    "<leader>tt",
+    "<cmd>lua require('plugins.lsp.utils').toggle_virtual_text()<cr>",
+    { desc = "Toggle Virtualtext" }
+)
+map(
+    "n",
+    "<leader>tS",
+    "<cmd>SymbolsOutline<cr>",
+    { desc = "Toggle SymbolsOutline" }
+)
 
 -- Comment Box
-map("v", "<leader>bb", "<cmd>CBllbox5<cr>", { desc = "left aligned fixed size box with left aligned text" })
+map(
+    "v",
+    "<leader>bb",
+    "<cmd>CBllbox5<cr>",
+    { desc = "left aligned fixed size box with left aligned text" }
+)
 
-map("v", "<leader>br", "<cmd>CBrrbox5<cr>", { desc = "left aligned fixed size box with left aligned text" })
+map(
+    "v",
+    "<leader>br",
+    "<cmd>CBrrbox5<cr>",
+    { desc = "left aligned fixed size box with left aligned text" }
+)
 
-map("v", "<leader>bc", "<cmd>CBccbox5<cr>", { desc = " center aligned fixed size box with center aligned text" })
+map(
+    "v",
+    "<leader>bc",
+    "<cmd>CBccbox5<cr>",
+    { desc = " center aligned fixed size box with center aligned text" }
+)
 
 map("i", "<M-l>", "<cmd>CBcline10<cr>", { desc = "centered line" })
 
@@ -80,106 +111,129 @@ local wk = require("which-key")
 
 -- register non leader based mappings
 wk.register({
-	sa = "Add surrounding",
-	sd = "Delete surrounding",
-	sh = "Highlight surrounding",
-	sn = "Surround update n lines",
-	sr = "Replace surrounding",
-	sF = "Find left surrounding",
-	sf = "Replace right surrounding",
-	ss = { "<cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.single_character)<cr>", "Jump to character" },
-	st = { "<cmd>lua require('tsht').nodes()<cr>", "TS hint textobject" },
+    sa = "Add surrounding",
+    sd = "Delete surrounding",
+    sh = "Highlight surrounding",
+    sn = "Surround update n lines",
+    sr = "Replace surrounding",
+    sF = "Find left surrounding",
+    sf = "Replace right surrounding",
+    ss = {
+        "<cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.single_character)<cr>",
+        "Jump to character",
+    },
+    st = { "<cmd>lua require('tsht').nodes()<cr>", "TS hint textobject" },
 })
 
 --  +-------------------------------------------------------------------------+
 
 -- Register leader based mappings
 wk.register({
-	["<tab>"] = { "<cmd>e#<cr>", "Prev buffer" },
-	b = {
-		name = "Comment Box",
-		b = {
-			"<cmd>CBllbox5<cr>",
-			"left aligned fixed size box with left aligned text",
-		},
+    ["<tab>"] = { "<cmd>e#<cr>", "Prev buffer" },
+    b = {
+        name = "Comment Box",
+        b = {
+            "<cmd>CBllbox5<cr>",
+            "left aligned fixed size box with left aligned text",
+        },
 
-		c = {
-			"<cmd>CBccbox5<cr>",
-			"center aligned fixed size box with center aligned text",
-		},
+        c = {
+            "<cmd>CBccbox5<cr>",
+            "center aligned fixed size box with center aligned text",
+        },
 
-		r = {
-			"<cmd>CBrrbox5<cr>",
-			"right aligned fixed size box with right aligned text",
-		},
+        r = {
+            "<cmd>CBrrbox5<cr>",
+            "right aligned fixed size box with right aligned text",
+        },
 
-		l = {
-			"<cmd>CBcline10<cr>",
-			"centered line",
-		},
-	},
+        l = {
+            "<cmd>CBcline10<cr>",
+            "centered line",
+        },
+    },
 
-	--  +-------------------------------------------------------------------------+
+    --  +-------------------------------------------------------------------------+
 
-	B = {
-		name = "Buffers",
-		b = {
-			"<cmd>Telescope buffers<cr>",
-			"Find buffer",
-		},
-		D = {
-			"<cmd>%bd|e#|bd#<cr>",
-			"Close all but the current buffer",
-		},
-		d = { "<cmd>Bdelete<cr>", "Close buffer" },
-	},
+    B = {
+        name = "Buffers",
+        b = {
+            "<cmd>Telescope buffers<cr>",
+            "Find buffer",
+        },
+        D = {
+            "<cmd>%bd|e#|bd#<cr>",
+            "Close all but the current buffer",
+        },
+        d = { "<cmd>Bdelete<cr>", "Close buffer" },
+    },
 
-	--  +-------------------------------------------------------------------------+
+    --  +-------------------------------------------------------------------------+
 
-	f = {
-		name = "Files",
-		b = { "<cmd>Telescope file_browser grouped=true<cr>", "File browser" },
-		f = { "<cmd>" .. require("utils.functions").project_files() .. "<cr>", "Find File" },
-		p = { "<cmd>Neotree reveal toggle<cr>", "Toggle Filetree" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-		s = { "<cmd>w<cr>", "Save Buffer" },
-	},
+    f = {
+        name = "Files",
+        b = { "<cmd>Telescope file_browser grouped=true<cr>", "File browser" },
+        f = {
+            "<cmd>" .. require("utils.functions").project_files() .. "<cr>",
+            "Find File",
+        },
+        p = { "<cmd>Neotree reveal toggle<cr>", "Toggle Filetree" },
+        r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+        s = { "<cmd>w<cr>", "Save Buffer" },
+    },
 
-	--  +-------------------------------------------------------------------------+
+    --  +-------------------------------------------------------------------------+
 
-	m = {
-		name = "Misc",
-		C = { "<cmd>:CBcatalog<cr>", "Commentbox Catalog" },
-		S = { "<cmd>:Cheatsheet <cr>", "Open Cheatsheet" },
-		l = { "<cmd>LuaSnipListAvailable <cr>", "List available snippets" },
-		p = { "<cmd>Lazy check<cr>", "Lazy check" },
-		u = { "<cmf>Lazy sync", "Update Packages" },
-	},
+    m = {
+        name = "Misc",
+        C = { "<cmd>:CBcatalog<cr>", "Commentbox Catalog" },
+        S = { "<cmd>:Cheatsheet <cr>", "Open Cheatsheet" },
+        l = { "<cmd>LuaSnipListAvailable <cr>", "List available snippets" },
+        p = { "<cmd>Lazy check<cr>", "Lazy check" },
+        u = { "<cmf>Lazy sync", "Update Packages" },
+    },
 
-	--  +-------------------------------------------------------------------------+
+    --  +-------------------------------------------------------------------------+
 
-	q = {
-		name = "Quickfix",
-		j = { "<cmd>cnext<cr>", "Next Quickfix Item" },
-		k = { "<cmd>cprevious<cr>", "Previous Quickfix Item" },
-		q = { "<cmd>lua require('utils.functions').toggle_qf()<cr>", "Toggle quickfix list" },
-		t = { "<cmd>TodoQuickFix<cr>", "Show TODOs" },
-	},
-	--   +---------------------------------------------------------------+
-	x = {
-		name = "Trouble",
-		x = { "<cmd>TroubleToggle<cr>", "Trouble Toggle" },
-		w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Trouble Workspace Diagnostics" },
-		d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Trouble Document Diagnostics" },
-		l = { "<cmd>TroubleToggle loclist<cr>", "Trouble Loc List" },
-		q = { "<cmd>TroubleToggle quickfix<cr>", "Trouble Quick Fix" },
-		s = { "<cmd>TroubleToggle lsp_references<cr>", "Trouble LSP References" },
-	},
-	--  +-------------------------------------------------------------------------+
+    q = {
+        name = "Quickfix",
+        j = { "<cmd>cnext<cr>", "Next Quickfix Item" },
+        k = { "<cmd>cprevious<cr>", "Previous Quickfix Item" },
+        q = {
+            "<cmd>lua require('utils.functions').toggle_qf()<cr>",
+            "Toggle quickfix list",
+        },
+        t = { "<cmd>TodoQuickFix<cr>", "Show TODOs" },
+    },
+    --   +---------------------------------------------------------------+
+    x = {
+        name = "Trouble",
+        x = { "<cmd>TroubleToggle<cr>", "Trouble Toggle" },
+        w = {
+            "<cmd>TroubleToggle workspace_diagnostics<cr>",
+            "Trouble Workspace Diagnostics",
+        },
+        d = {
+            "<cmd>TroubleToggle document_diagnostics<cr>",
+            "Trouble Document Diagnostics",
+        },
+        l = { "<cmd>TroubleToggle loclist<cr>", "Trouble Loc List" },
+        q = { "<cmd>TroubleToggle quickfix<cr>", "Trouble Quick Fix" },
+        s = {
+            "<cmd>TroubleToggle lsp_references<cr>",
+            "Trouble LSP References",
+        },
+    },
 
-	t = { name = "Toggles" },
-	-- hydra heads
-	s = { "Search" },
-	w = { "Windows" },
-	z = { "Spelling" },
+    p = {
+        "<cmd>ProjectTODOs<cr>",
+        "Toggle Project or Global TODOs",
+    },
+    --  +-------------------------------------------------------------------------+
+
+    t = { name = "Toggles" },
+    -- hydra heads
+    s = { "Search" },
+    w = { "Windows" },
+    z = { "Spelling" },
 }, { prefix = "<leader>", mode = { "n", "v" }, default_options })
