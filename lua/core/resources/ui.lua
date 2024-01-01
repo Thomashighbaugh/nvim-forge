2a0451231d5a0395d20db217825483a53789b8db
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2024-01-01 15:21:31 -0800

    add tree to readme

--- lua/core/resources/ui.lua
+++ lua/core/resources/ui.lua
@@ -363,31 +363,6 @@ return {
     end,
   },
 
-  {
-    "kosayoda/nvim-lightbulb",
-    opts = {
-      sign = {
-        enabled = true,
-        -- Priority of the gutter sign
-        priority = 20,
-      },
-      status_text = {
-        enabled = true,
-        -- Text to provide when code actions are available
-        text = "status_text",
-        -- Text to provide when no actions are available
-        text_unavailable = "",
-      },
-      autocmd = {
-        enabled = true,
-        -- see :help autocmd-pattern
-        pattern = { "*" },
-        -- see :help autocmd-events
-        events = { "LspAttach" },
-      },
-    },
-  },
-
   -- noicer ui
   {
     "folke/noice.nvim",
