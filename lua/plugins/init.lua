d6e8e1ffa4e00d02002a319ae70e88fb493ab902
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2025-01-14 04:06:07 -0800

    2025-01-14 04:06:07

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,24 +42,6 @@ return {
     --  │                          UTILS                           │
     --  ╰──────────────────────────────────────────────────────────╯
     {
-        'sudormrfbin/cheatsheet.nvim',
-        enabled = false,
-        keys = {
-            { '<space>cs', '<cmd>Cheatsheet<cr>', desc = 'Cheatsheet' },
-            { '<space>ce', '<cmd>CheatsheetEdit<cr>', desc = 'Cheatsheet Edit' },
-        },
-        dependencies = {
-            { 'nvim-telescope/telescope.nvim' },
-            { 'nvim-lua/popup.nvim' },
-            { 'nvim-lua/plenary.nvim' },
-        },
-        opts = {
-            bundled_plugin_cheatsheets = {
-                disabled = { 'gitsigns.nvim' },
-            },
-        },
-    },
-    {
         'smjonas/live-command.nvim',
         enabled = false,
         event = 'CmdlineEnter',
