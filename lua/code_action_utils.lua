f58c08bba7ca19c215ddcb94a288b7d24dbeb0f6
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-06-01 18:35:17 -0700

    2025-06-01 18:35:17

--- lua/code_action_utils.lua
+++ lua/code_action_utils.lua
@@ -4,7 +4,7 @@ local M = {}
 local lsp_util = vim.lsp.util
 
 function M.code_action_listener()
-    local context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
+    local context = { diagnostics = vim.diagnostic.get() }
     local params = lsp_util.make_range_params()
     params.context = context
 end
