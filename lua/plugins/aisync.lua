a82d6d62876ca6b53c7bc097e4976c8491fe73b8
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-06-13 10:09:38 -0700

    2025-06-13 10:09:38

--- lua/plugins/aisync.lua
+++ lua/plugins/aisync.lua
@@ -164,21 +164,21 @@ return {
                     opts = {
                         keymap = 'gh',
                         auto_generate_title = true,
-                        continue_last_chat = false,
-                        delete_on_clearing_chat = false,
+                        continue_last_chat = true,
+                        delete_on_clearing_chat = true,
                         picker = 'snacks',
                         enable_logging = false,
                         dir_to_save = vim.fn.stdpath('data') .. '/codecompanion-history',
                     },
                 },
-                -- mcphub = {
-                --   callback = "mcphub.extensions.codecompanion",
-                --   opts = {
-                --     make_vars = true,
-                --     make_slash_commands = true,
-                --     show_result_in_chat = true,
-                --   },
-                -- },
+                mcphub = {
+                    callback = 'mcphub.extensions.codecompanion',
+                    opts = {
+                        make_vars = true,
+                        make_slash_commands = true,
+                        show_result_in_chat = true,
+                    },
+                },
                 vectorcode = {
                     opts = {
                         add_tool = true,
@@ -198,10 +198,10 @@ return {
             },
         },
         keys = {
-            { '<leader>ic', '<cmd>CodeCompanion<cr>', desc = 'CodeCompanion' },
-            { '<leader>iC', '<cmd>CodeCompanionChat<cr>', desc = 'CodeCompanion Chat' },
-            { '<leader>ia', '<cmd>CodeCompanionActions<cr>', desc = 'CodeCompanion Actions' },
-            { '<leader>id', '<cmd>CodeCompanionCmd<cr>', desc = 'CodeCompanion CMD' },
+            { '<leader>ic', '<cmd>CodeCompanion<cr>', desc = 'CodeCompanion', mode = { 'n', 'v', 'x' } },
+            { '<leader>iC', '<cmd>CodeCompanionChat<cr>', desc = 'CodeCompanion Chat', mode = { 'n', 'v', 'x' } },
+            { '<leader>ia', '<cmd>CodeCompanionActions<cr>', desc = 'CodeCompanion Actions', mode = { 'n', 'v', 'x' } },
+            { '<leader>id', '<cmd>CodeCompanionCmd<cr>', desc = 'CodeCompanion CMD', mode = { 'n', 'v', 'x' } },
         },
         dependencies = {
             'j-hui/fidget.nvim',
@@ -233,7 +233,7 @@ return {
                         width = 95,
                         height = 10,
                         prompt = 'Prompt ', -- Prompt used for interactive LLM calls
-                        provider = 'snacks', -- Can be "default", "telescope", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+                        provider = 'telescope', -- Can be "default", "telescope", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
                         opts = {
                             show_default_actions = true, -- Show the default actions in the action palette?
                             show_default_prompt_library = true, -- Show the default prompt library in the action palette?
