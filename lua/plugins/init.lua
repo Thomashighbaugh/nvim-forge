56d8a9ac47883c94ba9ca9e644e93157b9bd3586
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-08-22 21:37:25 -0700

    2025-08-22 21:37:25

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -156,6 +156,19 @@ return {
         'fladson/vim-kitty',
         'MunifTanjim/nui.nvim',
     },
+    -- Lazy
+    {
+        '2kabhishek/nerdy.nvim',
+        dependencies = {
+            'folke/snacks.nvim',
+        },
+        cmd = 'Nerdy',
+        opts = {
+            max_recents = 30, -- Configure recent icons limit
+            add_default_keybindings = true, -- Add default keybindings
+            copy_to_clipboard = false, -- Copy glyph to clipboard instead of inserting
+        },
+    },
     -- colors
 
     -- ╭─────────────────────────────────────────────────────────╮
